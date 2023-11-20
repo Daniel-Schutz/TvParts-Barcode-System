217bd9fb0e58d7a7621d69c45f98083d1c107def
issac@getautonomi.com <issac@getautonomi.com>
2023-11-20 03:32:23 +0000

    Created CommonComponents.OpenOrderWHContainer; Edited theme.css and CommonComponents.WarehouseFulfillmentPanel

--- client_code/CommonComponents/WarehouseFulfillmentPanel/__init__.py
+++ client_code/CommonComponents/WarehouseFulfillmentPanel/__init__.py
@@ -10,5 +10,20 @@ class WarehouseFulfillmentPanel(WarehouseFulfillmentPanelTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.switch_to_empty_view()
 
     # Any code you write here will run before the form opens.
+
+  def switch_to_empty_view(self):
+    self.item_id_panel.visible = False
+    self.clear_item_btn.visible = False
+    self.needs_attention_btn.visible = False
+    self.item_scan_panel.visible = True
+    self.no_stock_btn.visible = True
+
+  def switch_to_scanned_view(self):
+    self.item_id_panel.visible = True
+    self.clear_item_btn.visible = True
+    self.needs_attention_btn.visible = True
+    self.item_scan_panel.visible = False
+    self.no_stock_btn.visible = False
