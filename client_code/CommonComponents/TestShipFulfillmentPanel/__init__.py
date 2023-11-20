edaf57d4491b27695c46e490ac916e590f8ac16d
issac@getautonomi.com <issac@getautonomi.com>
2023-11-20 04:47:11 +0000

    Created CommonComponents.NeedsAttentionParent.ItemTemplate6 and CommonComponents.TestShipFulfillmentPanel; Edited CommonComponents.NeedsAttentionParent

---
+++ client_code/CommonComponents/TestShipFulfillmentPanel/__init__.py
@@ -0,0 +1,27 @@
+from ._anvil_designer import TestShipFulfillmentPanelTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class TestShipFulfillmentPanel(TestShipFulfillmentPanelTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.switch_to_empty_view()
+
+    # Any code you write here will run before the form opens.
+
+  def switch_to_empty_view(self):
+    self.item_id_panel.visible = False
+    self.clear_item_btn.visible = False
+    self.item_scan_panel.visible = True
+    self.no_stock_btn.visible = True
+
+  def switch_to_scanned_view(self):
+    self.item_id_panel.visible = True
+    self.clear_item_btn.visible = True
+    self.item_scan_panel.visible = False
+    self.no_stock_btn.visible = False
