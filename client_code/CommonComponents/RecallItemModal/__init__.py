c3e218b55841f9d871d3d6564a1e123ecf8f0f3f
issac@getautonomi.com <issac@getautonomi.com>
2023-12-06 20:17:42 +0000

    Edited CommonComponents.RecallItemModal

--- client_code/CommonComponents/RecallItemModal/__init__.py
+++ client_code/CommonComponents/RecallItemModal/__init__.py
@@ -10,5 +10,29 @@ class RecallItemModal(RecallItemModalTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.cancel_btn.visible = False
+    self.get_box_btn.visible = False
+    self.clear_btn.visible = False
+    self.scan_input.focus()
 
     # Any code you write here will run before the form opens.
+
+  def on_scan_pressed_enter(self, **event_args):
+    self.cancel_btn.visible = True
+    self.get_box_btn.visible = True
+    self.clear_btn.visible = True
+    self.scan_input.enabled = False
+
+  def clear_btn_click(self, **event_args):
+    self.cancel_btn.visible = False
+    self.get_box_btn.visible = False
+    self.clear_btn.visible = False
+    self.scan_input.text = None
+    self.scan_input.enabled
+    self.scan_input.focus()
+
+  def cancel_btn_click(self, **event_args):
+    self.raise_event('x-close-alert', value=None)
+
+  def
+    
