25db624f71b8f85efabac665519e4136d764bcde
issac@getautonomi.com <issac@getautonomi.com>
2024-01-31 12:33:29 +0000

    Created CommonComponents.SingleProductEdit and CommonComponents.ProductEditor; Edited ManagementPages.ControlPanel

---
+++ client_code/CommonComponents/ProductEditor/__init__.py
@@ -0,0 +1,145 @@
+from ._anvil_designer import ProductEditorTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from anvil import js
+import anvil.media
+import math
+
+class ProductEditor(ProductEditorTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.type_dropdown.items = anvil.server.call('get_type_dropdown')
+    self.type_dropdown.selected_value = 'All Types'
+    self.repeating_panel_1.set_event_handler('x-product-selected', self.select_product)
+    self.page_size = 5
+    self.current_page = 0
+    self.max_pages = 0 #initialize
+    self.ttl_pg_lbl.text = 0
+    self.page_num_lbl.text = 0
+    self.matching_products = [] #placeholder for return
+    #self.load_page_data()
+    #self.temp_items_from_db()
+
+    # Any code you write here will run before the form opens.
+
+  def retrieve_prods_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    product_name = self.product_name_txbx.text
+    if self.name_contains_radio.selected:
+      product_name_search_type = self.name_contains_radio.value #which is "Contains"
+    elif self.name_exact_radio.selected:
+      product_name_search_type = self.name_exact_radio.value #which is "Contains"
+
+  #Issac - had to add these defaults to make the functions work
+    # desc_search_type = 'Contains'
+    # sku_search_type = 'Contains'
+    # product_name_search_type = 'Contains'
+
+    sku = self.product_sku_txbx.text
+    if self.sku_contains_radio.selected:
+      sku_search_type = self.sku_contains_radio.value
+    elif self.sku_exact_radio.selected:
+      sku_search_type = self.sku_exact_radio.value
+
+    product_description = self.product_desc_txbx.text
+    if self.desc_contains_radio_copy.selected:
+      desc_search_type = self.desc_contains_radio_copy.value
+    elif self.desc_exact_radio_copy.selected:
+      desc_search_type = self.desc_exact_radio_copy.value
+
+    if self.type_dropdown.selected_value:
+      type = self.type_dropdown.selected_value
+    else:
+      type = None
+
+    matching_products = anvil.server.call('run_product_explorer_query',
+                                          product_name,
+                                          product_name_search_type,
+                                          sku,
+                                          sku_search_type,
+                                          product_description,
+                                          desc_search_type,
+                                          type)
+
+
+    self.num_results_display.text = len(matching_products)
+    self.matching_products = matching_products
+
+
+    #Pagination variables
+    self.max_pages = math.ceil(len(self.matching_products)/self.page_size)
+    self.ttl_pg_lbl.text = self.max_pages
+    self.load_page_data()
+
+  def reset_search(self, **event_args):
+    self.name_contains_radio.selected = True
+    self.sku_contains_radio.selected = True
+    self.desc_contains_radio_copy.selected = True
+    self.type_dropdown.selected_value = 'All Types'
+    self.product_name_txbx.text = None
+    self.product_sku_txbx.text = None
+    self.product_desc_txbx.text = None
+    self.num_results_display.text = None
+    self.matching_products = []
+    self.ttl_pg_lbl.text = 0
+    self.num_results_display.text = 0
+    self.max_pages = 0
+    self.load_page_data()
+
+
+
+############# EVENTS #######################################
+  def main_submit_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  #helper for designing the repeater and pagination system
+  def temp_items_from_db(self):
+    self.repeating_panel_1.items = anvil.server.call('test_product_call')
+
+############### Pagination ##################################
+  def load_page_data(self):
+    start_index = self.current_page * self.page_size
+    end_index = start_index + self.page_size
+    # Load data for the current page
+    return_data = []
+    for i, row in enumerate(self.matching_products):
+      if i < start_index:
+        pass
+      elif i >= start_index and i < end_index:
+        return_data.append(row)
+      else:
+        break
+
+    self.repeating_panel_1.items = return_data
+    #self.repeater_1.items = self.data_for_repeater
+
+  def page_back_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.current_page <= 0:
+      pass
+    else:
+      self.current_page -= 1
+      self.page_num_lbl.text = self.current_page
+      self.load_page_data()
+
+  def page_next_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.current_page >= self.max_pages:
+      pass
+    else:
+      self.current_page += 1
+      self.page_num_lbl.text = self.current_page
+      self.load_page_data()
+
+######## Handle Product Selection ###################
+  def select_product(self, product, **event_args):
+    print("triggered the event")
+    self.selected_sku.text = product['sku']
+    #raising close alert event for modal operations
+    self.raise_event('x-close-alert', value=product)
---
+++ client_code/CommonComponents/SingleProductEdit/__init__.py
@@ -0,0 +1,43 @@
+from ._anvil_designer import SingleProductEditTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from anvil import js
+import anvil.media
+
+class SingleProductEdit(SingleProductEditTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.print_product_btn.visible = False
+    self.image_1.visible = False
+    self.init_components(**properties)
+
+
+    # Any code you write here will run before the form opens.
+
+######## Select Event #############################
+  def update_product_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    print("read the button click")
+    print(self.item['sku'])
+    n = Notification(f"The selected product is: {self.item['sku']}", style='info')
+    n.show()
+    self.parent.raise_event('x-product-selected', product=self.item)
+
+  def print_product_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    print("Image URL:",self.img_url)
+    js.call('printPage', self.img_url)
+
+  def generate_label_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    row = anvil.server.call('get_product_row_by_sku',self.item['sku'])
+    s3_obj_key = row['s3_object_key']
+    img_url = anvil.server.call('get_s3_image_url',s3_obj_key)
+    self.image_1.source = img_url
+    self.img_url = img_url
+    self.print_product_btn.visible = True
+    self.image_1.visible = True
